d333c2603143f791f7f6edb2519620a803182899
suntruxi <cristescu.ruxandra@yahoo.com>
2024-01-02 00:21:51 +0200

    added js-introduction

---
+++ js-introduction/script.js
@@ -0,0 +1,60 @@
+let sesiuneaCurenta = 12;
+let numarCursanti;
+
+sesiuneaCurenta = 13;
+console.log(sesiuneaCurenta);
+
+numarCursanti = 11;
+
+console.log(numarCursanti);
+
+let x = 10;
+let y = 23;
+const z = 3;
+let result = x * y + z * x;
+
+console.log(x);
+
+x--;
+x--;
+
+console.log(x);
+
+console.log(result);
+
+let numar1 = 44;
+let numar2 = " 44";
+
+console.log(numar1 == numar2);
+
+console.log(1 < 2 < 3);
+console.log(3 > 2 > 1);
+
+console.log(3 - 1);
+console.log(3 + 1);
+
+console.log("3" - 1);
+console.log("3" + 1);
+
+let numar3 = 25;
+let numar4 = "25";
+
+console.log(numar3 === numar4);
+
+let numar5 = 123;
+let numar6 = 123;
+
+console.log(numar5 !== numar6);
+
+console.log(0.1 + 0.2 == 0.3);
+console.log(0.1 + 0.2);
+
+console.log(0.5 + 0.1 == 0.6);
+
+console.log(true + true);
+console.log(true - false);
+
+console.log(Number("dasda"));
+console.log(typeof NaN);
+
+console.log(3 > 2 >= 1);
